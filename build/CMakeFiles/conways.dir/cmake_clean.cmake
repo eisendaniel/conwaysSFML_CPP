20c0c32ff262bc79911f75c90c273a9562cb1ce7
eisendaniel <danieleisen99@gmail.com>
2020-10-11 21:25:57 +1300

    converted project to pure cmake (ide agnostic) and included static lib

---
+++ build/CMakeFiles/conways.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/conways.dir/src/Cell.cpp.obj"
+  "CMakeFiles/conways.dir/src/main.cpp.obj"
+  "conways.exe"
+  "conways.exe.manifest"
+  "conways.pdb"
+  "libconways.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/conways.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/danie/projects/c_cpp/conwaysSFML_CPP
+# Build directory: C:/Users/danie/projects/c_cpp/conwaysSFML_CPP/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
